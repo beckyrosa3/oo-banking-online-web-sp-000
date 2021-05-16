c7541c009355005104766e8ad917403c427ea931
Becky Rosa <becky_m_rosa@yahoo.com>
2021-05-16 00:39:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,12 @@
 class Transfer=50
 
-  attr_reader :sender, :receiver, :status
+  attr_reader :sender, :receiver, :status, :amount
 
   def initialize(sender, receiver, status)
     @sender = sender
     @receiver = receiver
     @status = "pending"
+    @amount = 50
 
   end
 
